5c873fa5567fdf8e6f3e4030779b62d24024c6ff
hippoho <31813194+hippoho@users.noreply.github.com>
2017-09-10 12:52:51 +0800

    Update accountConfig.py

--- accountConfig.py
+++ accountConfig.py
@@ -5,8 +5,8 @@
 HUOBI = {
     "CNY_1":
         {
-            "ACCESS_KEY": "",
-            "SECRET_KEY": "",
+            "ACCESS_KEY": "a0f6efd8-9a1625ca-ceebfefd-f590b",
+            "SECRET_KEY": "aaf3b215-5e3a16ed-b5732237-48fe9",
             "SERVICE_API": "https://api.huobi.com/apiv3",
         },
     "USD_1":
@@ -32,8 +32,8 @@ BITVC = {
 OKCOIN = {
     "CNY_1":
         {
-            "ACCESS_KEY": "",
-            "SECRET_KEY": "",
+            "ACCESS_KEY": "62eaae92-1373-46e3-8c15-a80fed64ea7e",
+            "SECRET_KEY": "186520607FF38BB615E875D86D4535DF",
             "SERVICE_API": "https://www.okcoin.cn",  # okcoin国内站
         },
 
